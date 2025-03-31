0c7c1a0a59ecfa6fcb1f2d473a30b567c33e22e3
mflkee <makeevgleb86rus@gmail.com>
2025-03-31 11:08:10 +0500

    add

---
+++ cpp/learning/intro/02_functions/basic_functions.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <ostream>
+
+using namespace std;
+
+void greet() {
+  int x, b;
+  b = 15;
+  cin >> x;
+  cout << x + b << endl;
+}
+
+int main() { greet(); }
--- python/cherepashka/turtle_example.py
+++ python/cherepashka/turtle_example.py
@@ -2,20 +2,20 @@ import turtle
 
 # Создаем экран и черепашку
 screen = turtle.Screen()
-screen.bgcolor("white")
+screen.bgcolor("black")
 screen.title("Черепашка ходит по кругу")
 
 # Создаем черепашку
 t = turtle.Turtle()
+t.color("white")
 t.shape("turtle")
-t.speed(1)  # Устанавливаем скорость черепашки
+t.speed(10)  # Устанавливаем скорость черепашки
 
 # Движение по кругу
-radius = 100  # Радиус круга
+radius = 300  # Радиус круга
 t.penup()
 t.goto(0, -radius)  # Ставим черепашку в начальную точку
 t.pendown()
-
 t.circle(radius)  # Черепашка обходит круг
 
 # Завершаем программу по клику на экран
