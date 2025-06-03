12e14d066ec360996a1a1722a10a5b21f539fec2
mflkee <makeevgleb86rus@gmail.com>
2025-06-03 16:19:10 +0500

    upd

--- rust/basex/src/primitives/scalar.rs
+++ rust/basex/src/primitives/scalar.rs
@@ -1,6 +1,5 @@
-pub fn integer(){
-    let i8:i8 = 127;
-    let i16:i16 = 32767;
-    let i16:i16 = 32767;
-
+pub fn integer() {
+    let i8: i8 = 127;
+    let i16: i16 = 32767;
+    let i16: i16 = 32767;
 }
