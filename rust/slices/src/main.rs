0516fde6302ef81a39e46b31a8487c5ad6d5c0b4
Makeev Gleb Borisovich <makeevgleb86rus@gmail.com>
2025-12-03 09:40:13 +0500

    uod

--- rust/slices/src/main.rs
+++ rust/slices/src/main.rs
@@ -1,6 +1,9 @@
 mod no_use_slice;
 mod use_slice;
+mod test;
+
 fn main() {
-    no_use_slice::main();
-    use_slice::main();
+    // no_use_slice::main();
+    // use_slice::main();
+    test::main();
 }
