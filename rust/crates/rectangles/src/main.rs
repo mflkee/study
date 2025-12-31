72c1bde9de1e7f8461ff3675237239578613d020
Makeev Gleb Borisovich <makeevgleb86rus@gmail.com>
2025-12-31 16:17:44 +0500

    upd

--- rust/crates/rectangles/src/main.rs
+++ rust/crates/rectangles/src/main.rs
@@ -19,25 +19,45 @@
 // fn area(demensions:(u32,u32)) -> u32{
 //     demensions.0 * demensions.1
 // }
+
+// #[derive(Debug)]
+// struct Rectangle {
+//     width: u32,
+//     hight: u32,
+// }
+//
+// fn main(){
+//     let scale = 2;
+//     let rec1 = Rectangle{
+//         width: dbg!(30 * scale),
+//         hight: 50,
+//     };
+//     dbg!(&rec1);
+// }
+//
+// fn area(rectangle: &Rectangle) -> u32 {
+//         rectangle.width * rectangle.hight
+// }
+
 #[derive(Debug)]
-struct Rectangle {
+struct Rectangle{
     width: u32,
-    hight: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn has_area(&self) -> bool {
+        self.width > 0  && self.height > 0
+    }
 }
 
 fn main(){
-    let rec1 = Rectangle{
+    let rect1 = Rectangle {
         width: 30,
-        hight: 50,
+        height: 50,
     };
 
-    println!(
-        "The area of the recrtangle {} square pixels",
-        area(&rec1)
-    );
-    println!("rec1 is {rec1:?}");
-}
-
-fn area(rectangle: &Rectangle) -> u32 {
-        rectangle.width * rectangle.hight
+    if rect1.has_area(){
+        println!("The rectangle has a nonzero width; it is {} and height {}", rect1.width, rect1.height)
+    };
 }
