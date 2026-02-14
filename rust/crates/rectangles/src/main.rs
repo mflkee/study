ecab2d31a11a40efe7cfecd182ce2eabf20f96dc
Makeev Gleb Borisovich <makeevgleb86rus@gmail.com>
2026-02-14 14:35:27 +0500

    upd

--- rust/crates/rectangles/src/main.rs
+++ rust/crates/rectangles/src/main.rs
@@ -4,9 +4,26 @@ struct Rectangle{
     height: u32,
 }
 
+struct Square{
+    side: u32,
+}
+
 impl Rectangle {
     fn has_area(&self) -> bool {
         self.width > 0  && self.height > 0
+    }
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
+impl Square{
+    fn has_area(&self) -> bool {
+        self.side > 0
+    }
+
+    fn area(&self) -> u32 {
+        self.side * self.side
     }
 }
 
@@ -19,4 +36,16 @@ fn main(){
     if rect1.has_area(){
         println!("The rectangle has a nonzero width; it is {} and height {}", rect1.width, rect1.height)
     };
+
+    println!("Area rectangle is: {}", rect1.area());
+
+    let square1 = Square{
+        side: 10
+    };
+    
+    if square1.has_area(){
+        println!("The square has a nonzero width; side is {}", square1.side)
+    }
+    
+    println!("Area square is: {}", square1.area());
 }
