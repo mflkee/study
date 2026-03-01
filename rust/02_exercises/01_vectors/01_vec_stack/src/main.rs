972a60a0d15c1572d75670f035c5dc3391d2c888
Makeev Gleb Borisovich <makeevgleb86rus@gmail.com>
2026-03-01 17:31:24 +0500

    upd

--- rust/02_exercises/01_vectors/01_vec_stack/src/main.rs
+++ rust/02_exercises/01_vectors/01_vec_stack/src/main.rs
@@ -6,9 +6,9 @@ fn main() {
     println!("Complete TODO blocks in this file.");
 
     // Self-check (uncomment after implementation):
-    // let stack = build_stack(vec![10, 20, 30, 40]);
-    // let popped = pop_all(stack);
-    // println!("{popped:?}");
+    let stack = build_stack(vec![10, 20, 30, 40]);
+    let popped = pop_all(stack);
+    println!("{popped:?}");
     // Expected: [40, 30, 20, 10]
 }
 
@@ -16,9 +16,13 @@ fn main() {
 /// 1) Add all values from `input` to stack.
 /// 2) Return resulting stack.
 fn build_stack(input: Vec<i32>) -> Vec<i32> {
-    let _ = input;
     // Write your code below this line.
-    let stack: Vec<i32> = Vec::new();
+    let mut stack: Vec<i32> = Vec::new();
+
+    for value in input{
+        stack.push(value);
+    }
+
     stack
 }
 
@@ -27,8 +31,11 @@ fn build_stack(input: Vec<i32>) -> Vec<i32> {
 /// 2) Save popped values in order.
 /// 3) Return them as Vec<i32>.
 fn pop_all(mut stack: Vec<i32>) -> Vec<i32> {
-    let _ = &mut stack;
     // Write your code below this line.
-    let popped: Vec<i32> = Vec::new();
+    let mut popped: Vec<i32> = Vec::new();
+    while let Some(value) = stack.pop(){
+        popped.push(value);
+    };
+
     popped
 }
