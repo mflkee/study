7b5b7b8f4e9804292e9c196dd44081d316501665
mflkee <makeevgleb86@gmail.com>
2026-03-09 00:35:44 +0500

    upd

---
+++ rust/02_exercises/01_vectors/01_vec_stack/.cargo-target/debug/deps/ex_vec_stack-967f04efe6f2ee26.d
@@ -0,0 +1,5 @@
+/home/mflkee/study/rust/02_exercises/01_vectors/01_vec_stack/.cargo-target/debug/deps/ex_vec_stack-967f04efe6f2ee26.d: 02_exercises/01_vectors/01_vec_stack/src/main.rs
+
+/home/mflkee/study/rust/02_exercises/01_vectors/01_vec_stack/.cargo-target/debug/deps/ex_vec_stack-967f04efe6f2ee26: 02_exercises/01_vectors/01_vec_stack/src/main.rs
+
+02_exercises/01_vectors/01_vec_stack/src/main.rs:
---
+++ rust/02_exercises/01_vectors/01_vec_stack/.cargo-target/debug/ex_vec_stack.d
@@ -0,0 +1,1 @@
+/home/mflkee/study/rust/02_exercises/01_vectors/01_vec_stack/.cargo-target/debug/ex_vec_stack: /home/mflkee/study/rust/02_exercises/01_vectors/01_vec_stack/src/main.rs
