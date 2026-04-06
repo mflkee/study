d9180cd68b511738a66568279bbcc6a1e8f4ca5f
mflkee <makeevgleb86@gmail.com>
2026-04-06 18:30:05 +0500

    upd

---
+++ rust/01_basics/11_handing_errors/src/main.rs
@@ -0,0 +1,10 @@
+use std::fs::File;
+
+fn main() {
+    let greating_file_resoult = File::open("hello.txt");
+
+    let greating_file = match greating_file_resoult {
+        Ok(fo) =>
+        
+    };
+}
