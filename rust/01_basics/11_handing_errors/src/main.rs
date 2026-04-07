3386936d6a11a7431b6a60a3a35167357335767e
mflkee <makeevgleb86@gmail.com>
2026-04-07 16:15:08 +0500

    upd

--- rust/01_basics/11_handing_errors/src/main.rs
+++ rust/01_basics/11_handing_errors/src/main.rs
@@ -1,10 +1,21 @@
 use std::fs::File;
+use std::io::ErrorKind;
 
 fn main() {
-    let greating_file_resoult = File::open("hello.txt");
+    let path: &str = "hello.txt";
+    let greeting_file_result = File::open(path);
 
-    let greating_file = match greating_file_resoult {
-        Ok(fo) =>
-        
+    let greeting_file = match greeting_file_result {
+        Ok(fo) => fo,
+        Err(e) => match e.kind() {
+            ErrorKind::NotFound => match File::create(path) {
+                Ok(fc) => fc,
+                Err(e) => panic!("Error creating file {e:?}"),
+            },
+
+            _ => {
+                panic!("Problem opening file {e:?}")
+            }
+        },
     };
 }
