9997b38fd103a441b47fad1bdf96cd689f7609c2
mflkee <makeevgleb86rus@gmail.com>
2026-08-02 23:07:31 +0500

    upd

--- rust/01_basics/11_handling_errors/rb_vers/src/main.rs
+++ rust/01_basics/11_handling_errors/rb_vers/src/main.rs
@@ -3,25 +3,27 @@
 // Вместо expect() используем match для обработки разных видов ошибок
 // ErrorKind позволяет различать: файл не найден, нет прав, и т.д.
 
-use std::fs::File;
-use std::io::ErrorKind;
+use std::fs::OpenOptions;
+use std::io::Write;
+
 
 fn main() {
     let path: &str = "hello.txt";
-    let greeting_file_result = File::open(path);
 
-    let greeting_file = match greeting_file_result {
-        Ok(fo) => fo, // файл открылся успешно
-        Err(e) => match e.kind() {
-            // Файл не найден — создаём новый
-            ErrorKind::NotFound => match File::create(path) {
-                Ok(fc) => fc,
-                Err(e) => panic!("Ошибка создания файла {e:?}"),
-            },
-            // Другая ошибка (нет прав, и т.д.)
-            other_error => {
-                panic!("Проблема с открытием файла {other_error:?}")
-            }
-        },
+    let mut greeting_file = match OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .truncate(true)
+        .open(path){
+        Ok(file) => file,
+        Err(e) => panic!("Ошибка открытия файла {e:?}"),
     };
+
+    let text = String::from("Hello World!");
+    match greeting_file.write_all(text.as_bytes()) {
+        Ok(()) => println!("Текст успешно записан"),
+        Err(e) => panic!("Ошибка записи в файл: {e:?}"),
+    }
+
 }
