3f366ac3c9f8d77931b73939eed78f91ff275186
mflkee <mflkee@users.noreply.github.com>
2026-09-01 15:03:04 +0500

    project sync: study

--- python/algorithms/bs.py
+++ python/algorithms/bs.py
@@ -1,9 +1,18 @@
-class BinarySearch:
-    def bs(nums: list, arg: int) -> int:
-        
+target = 98
+nums: list[int] = [3, 5, 11, 15, 47, 98]
 
 
-nums = [[1 * i] for i in range(0,10000,3)]
-bs = BinarySearch()
+def bsearch(nums, target):
+    low = 0
+    hight: int = len(nums) - 1
+    while low <= hight:
+        mid: int = (low + hight) // 2
+        if nums[mid] == target:
+            return mid
+        if target < nums[mid]:
+            hight: int = mid - 1
+        else:
+            low: int = mid + 1
 
 
+print(bsearch(nums, target))
--- python/algorithms/mergeSort.py
+++ python/algorithms/mergeSort.py
@@ -1,15 +1,16 @@
 input_list = [5, 1, 4, 6, 2, 7, 3, 8]
 
+
 def merge_sort(list):
     if len(list) <= 1:
         return list
-            
-    mid = len(list) // 2 
+
+    mid = len(list) // 2
 
     left_half = merge_sort(list[:mid])
     right_half = merge_sort(list[mid:])
-
     result = []
+
     i = j = 0
     while i < len(left_half) and j < len(right_half):
         if left_half[i] < right_half[j]:
